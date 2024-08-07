90d541965ce7f5378c19e1b6ac5d6b698e9b7f6a
vinayak <vinayak20130@gmail.com>
2024-08-07 16:53:36 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,11 @@ const authRoutes = require('./routes/auth');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+// Middleware for parsing application/json
 app.use(express.json());
+
+// Middleware for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
 
 // Open CORS policy
 const corsOptions = {
@@ -25,11 +29,14 @@ app.use(cors(corsOptions));
 // Enable pre-flight across-the-board
 app.options('*', cors(corsOptions));
 
+// Serve static files
 app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
+// API routes
 app.use('/api/v1', todoRoutes);
 app.use('/api/auth', authRoutes);
 
+// Define a simple route
 app.get('', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
 });
@@ -38,6 +45,13 @@ app.get('/', (req, res) => {
   res.send('<h1>This is the home page</h1>');
 });
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
 });
