390a58ac33e976c03504127f040af4cc71cbd9d6
vinayak <vinayak20130@gmail.com>
2024-08-07 09:55:17 +0530

    test

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const path = require('path'); // Import the path module
+const path = require('path');
 const dbConnect = require('./config/database');
 const todoRoutes = require('./routes/todo');
 const authRoutes = require('./routes/auth');
@@ -10,27 +10,21 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
-app.use(cors({
-  origin: 'https://98-gfgwb9cjgfhsdecj.southeastasia-01.azurewebsites.net',
-  credentials: true
-}));
+app.use(cors());
 
-// Serve static files from the frontend build directory
-app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
 
-// API routes
 app.use('/api/v1', todoRoutes);
 app.use('/api/auth', authRoutes);
 
-// Serve frontend index.html for all other routes
+// The "catchall" handler: for any request that doesn't match one above, send back index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
 });
 
-// Connect to the database
 dbConnect();
