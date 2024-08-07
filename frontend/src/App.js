6a1625915165fd02a9ca8f4e3935b9203b289a79
vinayak <vinayak20130@gmail.com>
2024-08-07 18:55:46 +0530

    testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
@@ -8,16 +13,24 @@ import Notes from './pages/Notes';
 import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
-  const clientId = "781927388309-9424rc0fqhojghcqeud0b4p26pnmvop5.apps.googleusercontent.com";
+  const clientId =
+    '781927388309-9424rc0fqhojghcqeud0b4p26pnmvop5.apps.googleusercontent.com';
 
   return (
     <GoogleOAuthProvider clientId={clientId}>
-      <Router basename="/98-gfgwb9cjgfhsdecj.southeastasia-01.azurewebsites.net/">
+      <Router basename="https://98-gfgwb9cjgfhsdecj.southeastasia-01.azurewebsites.net/">
         <div className="App">
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/notes" element={<ProtectedRoute><Notes /></ProtectedRoute>} />
+            <Route
+              path="/notes"
+              element={
+                <ProtectedRoute>
+                  <Notes />
+                </ProtectedRoute>
+              }
+            />
             <Route path="/" element={<Navigate to="/login" />} />
           </Routes>
         </div>
